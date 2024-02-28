7337d2d71ec9b176c311381203cbe42a26f91c95
Anny <113927867+AnnyHoo@users.noreply.github.com>
2024-02-28 13:50:11 +0200

    Main program v01

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ShirtColorChange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxColorCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(63, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1068, 543);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1159, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1159, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+//            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBoxColorCode
+            // 
+            this.textBoxColorCode.Location = new System.Drawing.Point(1183, 380);
+            this.textBoxColorCode.Multiline = true;
+            this.textBoxColorCode.Name = "textBoxColorCode";
+            this.textBoxColorCode.Size = new System.Drawing.Size(148, 95);
+            this.textBoxColorCode.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1231, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "color code";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1473, 702);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxColorCode);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxColorCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ShirtColorChange
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImageCopy; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                   
+                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+                    originalImageCopy = new Bitmap(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null && originalImageCopy != null)
+            {
+               
+                string colorCode = textBoxColorCode.Text;
+
+               
+                if (!string.IsNullOrWhiteSpace(colorCode))
+                {
+                    
+                    if (ColorTranslator.FromHtml(colorCode) is Color targetColor)
+                    {
+                      
+                        Bitmap newImage = new Bitmap(originalImageCopy);
+
+                       
+                        for (int x = 0; x < originalImageCopy.Width; x++)
+                        {
+                            for (int y = 0; y < originalImageCopy.Height; y++)
+                            {
+                             
+                                Color pixelColor = originalImageCopy.GetPixel(x, y);
+
+                               
+                                float hue = targetColor.GetHue();
+                                float saturation = pixelColor.GetSaturation();
+                                float brightness = pixelColor.GetBrightness();
+
+                                Color newPixelColor = ColorHelper.ChangeHue(pixelColor, hue);
+
+                                newImage.SetPixel(x, y, newPixelColor);
+                            }
+                        }
+                        pictureBox1.Image = newImage;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid color code. Please enter a valid HTML color code.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a color code.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an image first.");
+            }
+        }
+
+    }
+
+    public static class ColorHelper
+    {
+        public static Color ChangeHue(Color color, float targetHue)
+        {
+            float saturation = color.GetSaturation();
+            float brightness = color.GetBrightness();
+
+            return FromAhsb(color.A, targetHue, saturation, brightness);
+        }
+
+        private static Color FromAhsb(int alpha, float hue, float saturation, float brightness)
+        {
+            int r, g, b;
+
+            if (saturation == 0)
+            {
+                r = g = b = (int)(brightness * 255.0 + 0.5);
+            }
+            else
+            {
+                hue /= 60; // Convert hue to the range [0, 6)
+                int i = (int)Math.Floor(hue);
+                float f = hue - i;
+
+                float p = brightness * (1.0f - saturation);
+                float q = brightness * (1.0f - saturation * f);
+                float t = brightness * (1.0f - saturation * (1.0f - f));
+
+                switch (i)
+                {
+                    case 0:
+                        r = (int)(brightness * 255.0 + 0.5);
+                        g = (int)(t * 255.0 + 0.5);
+                        b = (int)(p * 255.0 + 0.5);
+                        break;
+
+                    case 1:
+                        r = (int)(q * 255.0 + 0.5);
+                        g = (int)(brightness * 255.0 + 0.5);
+                        b = (int)(p * 255.0 + 0.5);
+                        break;
+
+                    case 2:
+                        r = (int)(p * 255.0 + 0.5);
+                        g = (int)(brightness * 255.0 + 0.5);
+                        b = (int)(t * 255.0 + 0.5);
+                        break;
+
+                    case 3:
+                        r = (int)(p * 255.0 + 0.5);
+                        g = (int)(q * 255.0 + 0.5);
+                        b = (int)(brightness * 255.0 + 0.5);
+                        break;
+
+                    case 4:
+                        r = (int)(t * 255.0 + 0.5);
+                        g = (int)(p * 255.0 + 0.5);
+                        b = (int)(brightness * 255.0 + 0.5);
+                        break;
+
+                    case 5:
+                        r = (int)(brightness * 255.0 + 0.5);
+                        g = (int)(p * 255.0 + 0.5);
+                        b = (int)(q * 255.0 + 0.5);
+                        break;
+
+                    default:
+                        r = g = b = (int)(brightness * 255.0 + 0.5);
+                        break;
+                }
+            }
+
+            return Color.FromArgb(alpha, r, g, b);
+        }
+    }
+}
